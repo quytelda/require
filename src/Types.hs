00babfbd437b01b9971732f6781c9a5ce1b98840
Quytelda Kahja <quytelda@tamalin.org>
2025-08-30 22:11:46 -0700

    Eliminate Action type

--- src/Types.hs
+++ src/Types.hs
@@ -43,17 +43,12 @@ defaultBankAssets = Assets
   , stockAssets = Map.fromList $ zip [Triangle ..] (repeat 25)
   }
 
-data Action
-  = DrawAction (Maybe Coord) -- ^ Draw a tile
-  | PlayAction Coord -- ^ Put a tile on the board
-  | DiscardAction Coord -- ^ Discard an unusable tile
-  | ReturnAction Coord -- ^ Return a pile to the pool
-  | MarkerAction (Maybe Coord) -- ^ Place or remove a company marker tile
-  | MoneyAction Money -- ^ Take or return money
-  | StockAction Company Int -- ^ Take or return stocks
-  deriving (Eq, Show)
-
--- | An event represents something happening in the game that needs to
--- be shared with all players.
-data Event = Event PlayerId Event
+data Event
+  = DrawEvent    PlayerId (Maybe Coord) -- ^ Draw a tile
+  | PlayEvent    PlayerId Coord -- ^ Put a tile on the board
+  | DiscardEvent PlayerId Coord -- ^ Discard an unusable tile
+  | ReturnEvent  PlayerId Coord -- ^ Return a pile to the pool
+  | MarkerEvent  PlayerId (Maybe Coord) -- ^ Place or remove a company marker tile
+  | MoneyEvent   PlayerId Money -- ^ Take or return money
+  | StockEvent   PlayerId Company Int -- ^ Take or return stocks
   deriving (Eq, Show)
