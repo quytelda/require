26bec2042ce08badab4a03f37ca77a48efd43350
Quytelda Kahja <quytelda@tamalin.org>
2025-09-07 19:44:51 -0700

    Rewrite Server module

--- src/Server.hs
+++ src/Server.hs
@@ -1,95 +1,19 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings   #-}
 
 module Server where
 
 import           Conduit
+import           Control.Concurrent.Async
 import           Control.Concurrent.STM
 import           Control.Monad
+import           Data.ByteString          (ByteString)
 import           Data.Conduit.Network
-import           Data.Map.Strict        (Map)
-import qualified Data.Map.Strict        as Map
+import           Data.Map.Strict          (Map)
+import qualified Data.Map.Strict          as Map
 
 import           Protocol
 import           Types
-
--- | Represents a connection with a player's client.
-data Player = Player
-  { playerId  :: PlayerId
-  , playerApp :: AppData
-  }
-
-playerSource
-  :: (MonadIO m, MonadThrow m)
-  => Player
-  -> ConduitT i Event m ()
-playerSource Player{..} = appSource playerApp .| parseEvents
-
-playerSink
-  :: (MonadIO m, PrimMonad m)
-  => Player
-  -> ConduitT Event o m ()
-playerSink Player{..} = renderEvents .| appSink playerApp
-
-data Server = Server
-  { serverPlayers   :: TVar (Map PlayerId Player)
-  , serverRecvQueue :: TQueue Event
-  }
-
-newServer :: IO Server
-newServer =
-  Server
-  <$> newTVarIO Map.empty
-  <*> newTQueueIO
-
-broadcastSink
-  :: (MonadIO m, PrimMonad m)
-  => Server
-  -> ConduitT Event Void m ()
-broadcastSink Server{..} =
-  void
-  $ liftIO (readTVarIO serverPlayers)
-  >>= sequenceSinks . fmap playerSink
-
-newPlayerId :: Server -> STM PlayerId
-newPlayerId server = do
-  players <- readTVar (serverPlayers server)
-  return $
-    if null players
-    then 1
-    else 1 + maximum (Map.keys players)
-
-registerPlayer :: Server -> AppData -> STM Player
-registerPlayer server appData = do
-  pid <- newPlayerId server
-  let player = Player pid appData
-  modifyTVar' (serverPlayers server) $ Map.insert pid player
-  return player
-
-unregisterPlayer :: Server -> Player -> STM ()
-unregisterPlayer server = modifyTVar' (serverPlayers server) . Map.delete . playerId
-
--- | Begin accepting connections from clients
-runServer :: IO ()
-runServer = do
-  server <- newServer
-  runTCPServer (serverSettings 11073 "*") $ \appData -> do
-
-    -- Registration
-    player@Player{..} <- atomically $ registerPlayer server appData
-    putStrLn $ "Player #" <> show playerId <> " connected"
-    runConduitRes
-      $ yield (HelloEvent playerId)
-      .| playerSink player
-
-    -- Stream incoming events to the global incoming queue.
-    runConduitRes
-      $ playerSource player
-      .| sinkTQueue (serverRecvQueue server)
-
-    -- TODO: Gracefully exit on connection errors and other issues.
-    atomically $ unregisterPlayer server player
-    putStrLn $ "Player #" <> show playerId <> " finished"
 
 sinkTQueue :: MonadIO m => TQueue a -> ConduitT a o m ()
 sinkTQueue queue = awaitForever $ liftIO . atomically . writeTQueue queue
@@ -97,3 +21,57 @@ sinkTQueue queue = awaitForever $ liftIO . atomically . writeTQueue queue
 sourceTQueue :: MonadIO m => TQueue a -> ConduitT i a m ()
 sourceTQueue queue =
   forever $ (liftIO . atomically . readTQueue) queue >>= yield
+
+data Server = Server
+  { uidSource :: TVar PlayerId
+  , clients   :: TVar (Map PlayerId (TQueue Event))
+  , recvQueue :: TQueue Event
+  }
+
+newServer :: IO Server
+newServer =
+  Server
+  <$> newTVarIO 0
+  <*> newTVarIO Map.empty
+  <*> newTQueueIO
+
+-- | Generate a new 'PlayerId' guaranteed to be unique for this
+-- 'Server'.
+newUID :: Server -> IO PlayerId
+newUID server = atomically $ do
+  modifyTVar' (uidSource server) (+1)
+  u <- readTVar (uidSource server)
+  return u
+
+-- | Broadcast an event to all clients.
+broadcast :: MonadIO m => Server -> ConduitT Event Void m ()
+broadcast server = awaitForever $ \x -> yield x .| sendAll
+  where
+    sendAll = liftIO (readTVarIO server.clients)
+              >>= sequenceSinks . fmap sinkTQueue
+
+-- | Begin accepting connections from clients
+runServer :: IO ()
+runServer = do
+  server <- newServer
+  concurrently_
+    (runConduit $ sourceTQueue server.recvQueue .| iterMC print .| broadcast server)
+    (runTCPServer (serverSettings 11073 "127.0.0.1") $ \app -> do
+        uid <- newUID server
+        sendQueue <- newTQueueIO
+        atomically $ modifyTVar' server.clients $ Map.insert uid sendQueue
+        putStrLn
+          $ "New connection from "
+          <> show (appSockAddr app)
+          <> maybe mempty (\s -> " (" <> show s <> ")") (appLocalAddr app)
+          <> ", UID: "
+          <> show uid
+
+        race_
+          (runConduit $ appSource app .| parseEvents .| sinkTQueue server.recvQueue)
+          (runConduit $ sourceTQueue sendQueue .| renderEvents .| appSink app)
+
+        putStrLn $ "Client disconnected, UID: " <> show uid
+        atomically $ modifyTVar' server.clients $ Map.delete uid
+    )
+
