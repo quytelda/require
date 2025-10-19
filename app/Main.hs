af3eadcb9d05cb832e6faa059f19c37c530b758b
Quytelda Kahja <quytelda@tamalin.org>
2025-10-18 21:54:58 -0700

    Add -p/--port option to change port

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,31 @@
 module Main (main) where
 
 import           Control.Concurrent.Async
+import           Control.Monad
 import qualified Data.Text.Lazy.Builder.Int as TBI
+import           System.Console.GetOpt
+import           System.Environment
 
 import           Server
 import           Types
 
+options :: [OptDescr (ServerState -> ServerState)]
+options =
+  [ Option "p" ["port"]
+    (ReqArg (\cs server -> server {serverPort = read cs}) "PORT")
+    "The TCP port the server will listen on (default 11073)"
+  ]
+
 main :: IO ()
 main = do
-  server <- newServerState
+  args <- getArgs
+  let (fs, _, errs) = getOpt RequireOrder options args
+      applyOptions = foldl (.) id fs
+
+  unless (null errs) $
+    mapM_ putStrLn errs
+
+  server <- applyOptions <$> newServerState
   putBuilderLn
     $ "Starting new server on port " <> TBI.decimal (serverPort server)
     <> " with server ID: " <> TBI.decimal (serverId server)
